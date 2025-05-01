4d8a6bf8e4ee20de9bc7dda2f041b9f3b0accbc3
JoaoFCosta <joaofiamenguicosta@gmail.com>
2025-05-01 17:50:04 -0300

    Adicionando botão para remover cartões salvos

--- devSteam-2025/src/pages/Pagamentos.jsx
+++ devSteam-2025/src/pages/Pagamentos.jsx
@@ -38,6 +38,12 @@ const Pagamentos = () => {
     }
   };
 
+  const handleRemoveCard = (indexToRemove) => {
+    const updatedCards = cards.filter((_, index) => index !== indexToRemove);
+    setCards(updatedCards);
+    localStorage.setItem("cards", JSON.stringify(updatedCards)); // Atualiza o localStorage
+  };
+
   useEffect(() => {
     // Atualiza o localStorage sempre que a lista de cartões mudar
     localStorage.setItem("cards", JSON.stringify(cards));
@@ -47,85 +53,92 @@ const Pagamentos = () => {
     <>
       <PerfilHeader />
       <div className="p-5">
-        <div className="col-md-6 text-start">
-          <h3>Cartões Salvos</h3>
-          <hr />
-          {cards.length > 0 ? (
-            <ul>
-              {cards.map((card, index) => (
-                <li key={index}>
-                  <strong>Número:</strong> **** **** ****{" "}
-                  {card.cardNumero.slice(-4)} <br />
-                  <strong>Titular:</strong> {card.cardTitular} <br />
-                  <strong>Validade:</strong> {card.dataExpirar}
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p>Nenhum cartão salvo.</p>
-          )}
-        </div>
-        <div className="col-md-6">
-        <hr />
-          <h3>Adicionar Novo Cartão</h3>
-          <hr />
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              handleAddCard();
-            }}
-          >
-            <div className="mb-3">
-              <label className="form-label">Número do Cartão:</label>
-              <input
-                type="text"
-                name="cardNumero"
-                value={newCard.cardNumero}
-                onChange={handleInputChange}
-                maxLength="16"
-                className="form-control"
-                required
-              />
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Nome do Titular:</label>
-              <input
-                type="text"
-                name="cardTitular"
-                value={newCard.cardTitular}
-                onChange={handleInputChange}
-                className="form-control"
-                required
-              />
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Data de Validade:</label>
-              <input
-                type="text"
-                name="dataExpirar"
-                value={newCard.dataExpirar}
-                onChange={handleInputChange}
-                placeholder="MM/AA"
-                className="form-control"
-                required
-              />
-            </div>
-            <div className="mb-3">
-              <label className="form-label">CVV:</label>
-              <input
-                type="text"
-                name="cvv"
-                value={newCard.cvv}
-                onChange={handleInputChange}
-                maxLength="3"
-                className="form-control"
-                required
-              />
-            </div>
-            <button type="submit" className="btn btn-primary w-100 mt-4">
-              Salvar Cartão
-            </button>
-          </form>
+        <div className="row">
+          <div className="col-md-6 text-start">
+            <h3>Cartões Salvos</h3>
+            <hr />
+            {cards.length > 0 ? (
+              <ul>
+                {cards.map((card, index) => (
+                  <li key={index}>
+                    <strong>Número:</strong> **** **** ****{" "}
+                    {card.cardNumero.slice(-4)} <br />
+                    <strong>Titular:</strong> {card.cardTitular} <br />
+                    <strong>Validade:</strong> {card.dataExpirar}
+                    <button
+                      className="btn btn-danger btn-sm mx-4 mb-3"
+                      onClick={() => handleRemoveCard(index)}
+                    >
+                      Remover
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>Nenhum cartão salvo.</p>
+            )}
+          </div>
+          <div className="col-md-6">
+            <h3>Adicionar Novo Cartão</h3>
+            <hr />
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                handleAddCard();
+              }}
+            >
+              <div className="mb-3">
+                <label className="form-label">Número do Cartão:</label>
+                <input
+                  type="text"
+                  name="cardNumero"
+                  value={newCard.cardNumero}
+                  onChange={handleInputChange}
+                  maxLength="16"
+                  className="form-control"
+                  required
+                />
+              </div>
+              <div className="mb-3">
+                <label className="form-label">Nome do Titular:</label>
+                <input
+                  type="text"
+                  name="cardTitular"
+                  value={newCard.cardTitular}
+                  onChange={handleInputChange}
+                  className="form-control"
+                  required
+                />
+              </div>
+              <div className="mb-3">
+                <label className="form-label">Data de Validade:</label>
+                <input
+                  type="text"
+                  name="dataExpirar"
+                  value={newCard.dataExpirar}
+                  onChange={handleInputChange}
+                  placeholder="MM/AA"
+                  className="form-control"
+                  required
+                />
+              </div>
+              <div className="mb-3">
+                <label className="form-label">CVV:</label>
+                <input
+                  type="text"
+                  name="cvv"
+                  value={newCard.cvv}
+                  onChange={handleInputChange}
+                  maxLength="3"
+                  className="form-control"
+                  required
+                />
+              </div>
+              <button type="submit" className="btn btn-primary w-100 mt-4">
+                Salvar Cartão
+              </button>
+            </form>
+          </div>
         </div>
       </div>
       <Footer />
